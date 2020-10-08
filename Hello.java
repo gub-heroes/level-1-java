19d28354109c4b0e8bceab7277192eeb344dfbb2
Mohammad Nazmul Hossain <57767915+Naz365@users.noreply.github.com>
2020-10-08 15:55:59 +0600

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String args[]) {
+      
+ //print the string "Hello people" on screen
+      System.out.println(" Hello people!" );
+    }
+}
---
+++ OutputVariable.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int value = 10;
+      char x;
+      x = 'A' ;
+      System.out.println( value );
+      System.out.println("the value of x is=" + x );
+    }
+}
