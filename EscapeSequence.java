6943fed8f213d94bc3b4bbfb9624fcb9d1058a0a
Mohammad Nazmul Hossain <57767915+Naz365@users.noreply.github.com>
2020-10-10 14:08:16 +0600

    Add files via upload

---
+++ EscapeSequence.java
@@ -0,0 +1,7 @@
+public class EscapeSequence {
+    public static void main(String[] args){
+        
+        System.out.print("\"\" \"\t\"\"\n\"\"  \"\t\"\"\n\"\"   \"\t\"\"\n\"\"    \" \"\" ");
+    
+    }
+}
