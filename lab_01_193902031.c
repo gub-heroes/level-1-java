21e8dd6f2b8716fdf6b3e7b98fb60cd207d36cc7
Mohammad Nazmul Hossain <57767915+Naz365@users.noreply.github.com>
2020-10-19 20:04:48 +0600

    Add files via upload

---
+++ lab_01_193902031.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int stack[365],size,top=-1;
+
+void push(int value)//
+{
+    if ( top == size -1 )
+    {
+        printf("oh no! Stack is full < Overflow Occur> %d\n",value);
+    }
+    else //
+    {
+        top +=1;
+        printf("Your push index is %d and element is : %d\n",top,value);
+        stack[top]=value;
+    }
+    //return push;
+}
+
+int main()
+{
+
+    int i;
+
+    printf("Enter the Stack size: ");
+    scanf("%d", &size);
+
+
+    printf("\n<<<< Here is your Stack >>>>\n\n");
+    printf("Your Stack is  Here : %d\n",size);
+
+    push(17);
+    push(5);
+    push(123);
+    push(25);
+    push(12);
+    push(87);
+
+
+
+    for (i=top; i>=0; i-=1)
+    {
+        printf("%d\n",stack[i]);
+    }
+}
